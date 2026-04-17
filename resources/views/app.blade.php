a5b96523fc61c167764ad5c4e8a016b3aef2b009
ragilpratama <ragilpratamaaa@gmail.com>
2026-04-17 09:23:53 +0700

    style: update application font family to Poppins

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title inertia>{{ config('app.name', 'GarmenApp') }}</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
         @routes
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @inertiaHead
