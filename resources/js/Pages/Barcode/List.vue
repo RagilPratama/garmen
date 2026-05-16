7303342502cfb07e886dfc68f53a3c475ef35c06
ragilpratama <ragilpratamaaa@gmail.com>
2026-05-16 23:35:08 +0700

    refactor(barcode): remove scan barcode button from list view
    - Remove "Scan Barcode" navigation link from barcode list page
    - Simplify action buttons to only show "Generate Baru" option
    - Clean up unused SVG icon markup for barcode scanner button

--- resources/js/Pages/Barcode/List.vue
+++ resources/js/Pages/Barcode/List.vue
@@ -14,17 +14,6 @@
                         </svg>
                         Generate Baru
                     </a>
-                    <a href="/barcode/scan" class="px-4 py-2 bg-green-500 hover:bg-green-600 text-white text-sm font-medium rounded-lg transition flex items-center gap-2">
-                        <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M12 4v1m6 11h2m-6 0h-2v4m0-11v3m0 0h.01M12 12h4.01M16 20h4M4 12h4m12 0h.01M5 8h2a1 1 0 001-1V5a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1zm12 0h2a1 1 0 001-1V5a1 1 0 00-1-1h-2a1 1 0 00-1 1v2a1 1 0 001 1zM5 20h2a1 1 0 001-1v-2a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1z"
-                            />
-                        </svg>
-                        Scan Barcode
-                    </a>
                 </div>
             </div>
 
